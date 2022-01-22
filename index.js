ab23cef6dc98d9c119aef519ff171eff1a191794
DzinaButko <butkodina@gmail.com>
2022-01-22 09:23:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+//to run node index.js
+//npm install -g nodemon
